7f87b75d64b7f3824e6f1247baa848c958bda9fc
Daniel Normandia <danielalbertnormandia@gmail.com>
2016-12-01 16:02:49 -0500

    add ajax calls and button functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
 
+var addEventListeners = function() {
+  $('.playerSearch').submit(function(event) {
+    var $playerName = $('#pSearch').val();
+    playerCall($playerName);
+    event.preventDefault();
+  });
+  $('.tournamentSearch').submit(function(event) {
+    var $tourneyName = $('#tSearch').val();
+    tourneyCall($tourneyName);
+    event.preventDefault();
+  });
+};
+addEventListeners();
 
-var makeCall = function(){
-    $.ajax({
-      url: '/data',
-      method: 'GET'
-    })
-    .fail(function(data) {
-      console.log('Error');
-    })
-    .done(function(data) {
-      console.log(data);
-    })
-  }
-makeCall();
+var playerCall = function(pName)  {
+  $.ajax({
+    url: '/pData/' + pName,
+    method: 'GET'
+  })
+  .fail(function(data) {
+    console.log('Error');
+  })
+  .done(function(data) {
+
+  });
+};
+
+var tourneyCall = function(tName) {
+  $.ajax({
+    url:'/tData/' + tName,
+    method: 'GET'
+  })
+  .fail(function(data) {
+    console.log('Error');
+  })
+  .done(function(data) {
+
+  });
+};
+
 
 
 
