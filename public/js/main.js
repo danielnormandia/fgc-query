5aa72b2beac74b1102376f745ed44e74b48f8c23
Daniel Normandia <danielalbertnormandia@gmail.com>
2016-12-02 13:31:56 -0500

    add single tourney ajax call

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ var addEventListeners = function() {
   });
   $('.tournamentSearch').submit(function(event) {
     var $tourneyName = $('#tSearch').val();
-    tourneyCall($tourneyName);
+    tournamentsCall($tourneyName);
     event.preventDefault();
   });
 };
@@ -23,7 +23,20 @@ var playerCall = function(pName)  {
     console.log('Error');
   })
   .done(function(data) {
+    appendPlayer(data);
+  });
+};
 
+var tournamentsCall = function(tsName) {
+  $.ajax({
+    url:'/tsData/' + tsName,
+    method: 'GET'
+  })
+  .fail(function(data) {
+    console.log('Error');
+  })
+  .done(function(data) {
+      appendTournaments(data);
   });
 };
 
@@ -36,12 +49,54 @@ var tourneyCall = function(tName) {
     console.log('Error');
   })
   .done(function(data) {
-
+      appendTournament(data);
   });
 };
 
+var appendPlayer = function(pObject) {
+    $('.playerSearch').hide();
+    var $playerDiv = $('<div class="player"></div>');
+    for(var prop in pObject) {
+      if(prop !== 'results' && prop !== 'rankings') {
+      var $playerProp = $('<p>'+ prop + ': ' + pObject[prop] + '</p>');
+      $playerDiv.append($playerProp);
+    }
+    $('body').append($playerDiv);
+  };
+}
+var appendTournaments = function(tObject) {
+  $('.tournamentSearch').hide();
+  var $tournamentsDiv = $('<div class="tournaments"></div>');
+  tObject.forEach(function(obj) {
+    var $tournamentName = $('<p>' + obj.value + '</p>');
+    $tournamentName.click(function() {
+      var $tourney = $(this).text();
+      tourneyCall($tourney);
+    })
+    $tournamentsDiv.append($tournamentName);
+  });
+  $('body').append($tournamentsDiv);
+};
 
-
+var appendTournament = function(singleTourneyObj) {
+  var $tournamentsDiv1 = $('div.tournaments');
+  $tournamentsDiv1.hide();
+  var obj = singleTourneyObj;
+  var $tournamentDiv = $('<div class="tournament"></div>');
+  var $tournamentList = $('<ul class="tList"></ul>')
+  var $tournamentId = $('<li>' + obj.id + '</li>')
+  var $tournamentName = $('<li>' + obj.name + "</li>");
+  var $tournamentCountry = $('<li>' + obj.country + '</li>');
+  var $tournamentGame = $('<li>' + obj.game + '</li>');
+  var $tournamentCoverage = $('<li>' + obj.coverage + '</li>');
+  $tournamentList.append($tournamentId)
+                 .append($tournamentName)
+                 .append($tournamentCountry)
+                 .append($tournamentGame)
+                 .append($tournamentCoverage);
+  $tournamentDiv.append($tournamentList);
+  $('body').append($tournamentDiv);
+};
 
 
 
