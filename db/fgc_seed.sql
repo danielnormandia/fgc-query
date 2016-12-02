5b6a0b6cff49c7d3887df907c00cdb1894ef6755
Daniel Normandia <danielalbertnormandia@gmail.com>
2016-12-02 13:31:00 -0500

    correct table columns

--- db/fgc_seed.sql
+++ db/fgc_seed.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS queries;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS queries CASCADE;
 
 CREATE TABLE Users (
    id SERIAL PRIMARY KEY,
@@ -7,12 +7,13 @@ CREATE TABLE Users (
    email VARCHAR(255) UNIQUE NOT NULL,
    joinDate timestamp NOT NULL,
    password_digest VARCHAR(255) NOT NULL
-   )
+   );
 
 CREATE TABLE queries(
-   user_id REFERENCES users,
+   id SERIAL PRIMARY KEY,
+   user_id integer REFERENCES users(id),
    term VARCHAR(255) NOT NULL,
    searchTime timestamp NOT NULL,
    type VARCHAR(1) NOT NULL
-   )
+   );
 
